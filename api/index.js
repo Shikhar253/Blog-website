d129af00840ab946670451b2319ea843b3323a01
shikhar <saxenashikhar01@gmail.com>
2021-07-28 23:46:36 +0530

    .

--- api/index.js
+++ api/index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const app=express();
 const dotenv = require("dotenv")
 const mongoose=require("mongoose")
+const authRoute=require("./routes/auth")
+const userRoute=require("./routes/users")
 
-dotenv.config()
+
+dotenv.config();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL, {
     useNewUrlParser: true, useUnifiedTopology: true,
     useCreateIndex:true,
 }).then(console.log("Connected to MongoDB")).catch(err=>console.log(err));
 
+app.use("/api/auth",authRoute);
+app.use("/api/users",userRoute);
 
-app.use("")
+//  app.use("/blog",(req,res)=>{
+//      console.log("hey this is blog url");
+//  })
 
-
+// console.log("Shikhar")
 
 app.listen("5000",()=>{
     console.log("Backend is running.");
